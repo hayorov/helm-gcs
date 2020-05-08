2b25d1b132ae254c07ff588adad3e47f1f845a8a
CarsonBull <Carson.bull@gmail.com>
2020-05-08 15:18:50 -0700

    Removing checks

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,17 +40,7 @@ else
 fi
 
 # Install bin
-rm -rf bin && mkdir bin
-
-tar xvf $filename -C bin > /dev/null 
-
-if [ $? -ne 0 ]
-then
-    echo "Error installing helm-gcs while unpacking archive"
-    exit 1
-fi
-
-rm -f $filename
+rm -rf bin && mkdir bin && tar xvf $filename -C bin > /dev/null && rm -f $filename
 
 echo "helm-gcs ${version} is correctly installed."
 echo
